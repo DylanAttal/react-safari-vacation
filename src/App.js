dfbcd5578360c3f322e0930db90587bdcfa78d5d
Dylan Attal <attal.dylan@gmail.com>
2018-12-20 08:59:07 -0500

    Connects each API endpoint with a button with an onClick in React UI.

--- src/App.js
+++ src/App.js
@@ -14,21 +14,21 @@ class App extends Component {
       total_count_LTB: ''
     }
   }
-  componentDidMount = () => {
+  displayAnimals = () => {
     axios.get('http://localhost:4567/Animals').then(response => {
       this.setState({
         animal_list: response.data.animals
       })
     })
   }
-  componentDidMount = () => {
+  displayAnimalsInJungle = () => {
     axios.get('http://localhost:4567/Animal/Jungle').then(response => {
       this.setState({
         animals_in_jungle: response.data.animals
       })
     })
   }
-  _click = () => {
+  deleteDesertAnimals = () => {
     axios.delete('http://localhost:4567/Animal/Desert').then(response => {
       this.setState({
         animals_in_desert: response.data.animals
@@ -36,14 +36,14 @@ class App extends Component {
       console.log(response.data)
     })
   }
-  componentDidMount = () => {
+  displayTotalCount = () => {
     axios.get('http://localhost:4567/Count').then(response => {
       this.setState({
         total_count: response.data.total_count_of_times_seen
       })
     })
   }
-  componentDidMount = () => {
+  displayCountofLTB = () => {
     axios.get('http://localhost:4567/Count/LTB').then(response => {
       this.setState({
         total_count_LTB: response.data.total_count_of_times_seen_LTB
@@ -58,19 +58,29 @@ class App extends Component {
           <h1>SAFARI VACATION</h1>
         </header>
         <h3>List of Animals</h3>
+        <button onClick={this.displayAnimals}>Display Animals</button>
         {this.state.animal_list.map((animal, index) => {
           return <p key={index}>{animal.species}</p>
         })}
         <h3>List of Animals in the Jungle</h3>
+        <button onClick={this.displayAnimalsInJungle}>
+          Display Animals in Jungle
+        </button>
         {this.state.animals_in_jungle.map((animal, index) => {
           return <p key={index}>{animal.species}</p>
         })}
         <h3>Delete the animals in the desert!</h3>
-        <button onClick={this._click}>Delete em!</button>
-        <p>{this.state.animals_in_desert} got deleted!</p>
+        <button onClick={this.deleteDesertAnimals}>
+          Delete Desert animals!
+        </button>
+        <p>{this.state.animals_in_desert}</p>
         <h3>Total count of animals seen:</h3>
+        <button onClick={this.displayTotalCount}>Display Total Count</button>
         <p>{this.state.total_count}</p>
         <h3>Total count of Lions, Tigers, and Bears:</h3>
+        <button onClick={this.displayCountofLTB}>
+          Display count of Lions, Tigers, and Bears
+        </button>
         <p>{this.state.total_count_LTB}</p>
       </div>
     )
