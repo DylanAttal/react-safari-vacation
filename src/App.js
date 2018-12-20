3e4b31738158eed470eedd2d7da6c3c2352f245f
Dylan Attal <attal.dylan@gmail.com>
2018-12-20 08:24:34 -0500

    Creates app. Links UI to API on localhost.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      animal_list: [''],
+      animals_in_jungle: [],
+      animals_in_desert: [],
+      total_count: '',
+      total_count_LTB: ''
+    }
+  }
+  componentDidMount = () => {
+    axios.get('http://localhost:4567/Animals').then(response => {
+      this.setState({
+        animal_list: response.data.animals
+      })
+    })
+  }
+  componentDidMount = () => {
+    axios.get('http://localhost:4567/Animal/Jungle').then(response => {
+      this.setState({
+        animals_in_jungle: response.data.animals
+      })
+    })
+  }
+  _click = () => {
+    axios.delete('http://localhost:4567/Animal/Desert').then(response => {
+      this.setState({
+        animals_in_desert: response.data.animals
+      })
+      console.log(response.data)
+    })
+  }
+  componentDidMount = () => {
+    axios.get('http://localhost:4567/Count').then(response => {
+      this.setState({
+        total_count: response.data.total_count_of_times_seen
+      })
+    })
+  }
+  componentDidMount = () => {
+    axios.get('http://localhost:4567/Count/LTB').then(response => {
+      this.setState({
+        total_count_LTB: response.data.total_count_of_times_seen_LTB
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>SAFARI VACATION</h1>
         </header>
+        <h3>List of Animals</h3>
+        {this.state.animal_list.map((animal, index) => {
+          return <p key={index}>{animal.species}</p>
+        })}
+        <h3>List of Animals in the Jungle</h3>
+        {this.state.animals_in_jungle.map((animal, index) => {
+          return <p key={index}>{animal.species}</p>
+        })}
+        <h3>Delete the animals in the desert!</h3>
+        <button onClick={this._click}>Delete em!</button>
+        <p>{this.state.animals_in_desert} got deleted!</p>
+        <h3>Total count of animals seen:</h3>
+        <p>{this.state.total_count}</p>
+        <h3>Total count of Lions, Tigers, and Bears:</h3>
+        <p>{this.state.total_count_LTB}</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
